1b00929e1fc8425fe3e458493ad5c7d6d93c27eb
22brunda <bsrinivas@qwinix.io>
2018-03-22 15:47:40 +0530

    completed task2

---
+++ JavaScript/frontview.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+  debugger
+  $("#aboutpage").hide();
+  $("#hme").click(function(){
+    debugger
+    $("#aboutpage").hide();
+    $("#homepage").show();
+  });
+
+  $("#abt").click(function(){
+    debugger
+    $("#homepage").hide();
+    $("#aboutpage").show();
+  });
+
+  $("#get").click(function(){
+    $("#aboutpage").show();
+    $("#homepage").hide();
+  });
+
+  $("#info").click(function(){
+    $("#aboutpage").show();
+    $("#homepage").hide();
+  });
+
+  $("#info1").click(function(){
+    $("#aboutpage").show();
+    $("#homepage").hide();
+  });
+
+  $("#info2").click(function(){
+    $("#aboutpage").show();
+    $("#homepage").hide();
+  });
+
+});
+
+function myFunction(){
+  $("#aboutpage").show();
+    $("#homepage").hide();
+
+}
